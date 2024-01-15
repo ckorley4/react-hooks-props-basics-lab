2a66bad432bf5fa3d934b0a9c45a9751dedb011e
ckorley4 <ckorley4uo@yahoo.com>
2024-01-15 08:38:28 -0500

    props basic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React from 'react'
 function Home(props) {
   return (
     <div id="home">
@@ -7,7 +6,7 @@ function Home(props) {
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
